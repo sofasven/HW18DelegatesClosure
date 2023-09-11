28a87ebc648bb18c4f37945dfb129544bd1d5d9e
Sofa <sofasven00@gmail.com>
2023-09-12 00:46:18 +0300

    Some changes

--- DelegatesClosureSendDataApp/DelegatesClosureSendDataApp/Controllers/ChangeBGVC.swift
+++ DelegatesClosureSendDataApp/DelegatesClosureSendDataApp/Controllers/ChangeBGVC.swift
@@ -22,7 +22,8 @@ class ChangeBGVC: UIViewController {
     @IBOutlet weak var blueTF: UITextField!
     @IBOutlet weak var opacity: UISlider!
     @IBOutlet weak var opacityTF: UITextField!
-    
+    @IBOutlet weak var hexColorTF: UITextField!
+
     var color: UIColor?
     var delegate: ColorUpdateProtocol?
     var changeColorWithClosure: ( (UIColor) -> (UIColor) )?
@@ -80,6 +81,26 @@ class ChangeBGVC: UIViewController {
         navigationController?.popViewController(animated: true)
     }
     
+    
+    
+    
+    @IBAction func hexColorAction(_ sender: UITextField) {
+        guard let hexValue = sender.text,
+              let color = UIColor(hex: hexValue),
+              let red = color.cgColor.components?[0],
+              let green = color.cgColor.components?[1],
+              let blue = color.cgColor.components?[2],
+              let alfa = color.cgColor.components?[3] else { return }
+        colorView.backgroundColor = color
+        redSlider.value = Float(red)
+        redTF.text = String(round((Double(red.description) ?? 0) * 10) / 10)
+        greenSlider.value = Float(green)
+        greenTF.text = String(round((Double(green.description) ?? 0) * 10) / 10)
+        blueSlider.value = Float(blue)
+        blueTF.text = String(round((Double(blue.description) ?? 0) * 10) / 10)
+        opacity.value = Float(alfa)
+                opacityTF.text = String(round((Double(alfa.description) ?? 0) * 10) / 10)
+    }
     
     func changeColor() {
         colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(opacity.value))
